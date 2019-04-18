1d988ae3ebea1d7f53eef42b4cc2d57729542ade
fortunate-jest-6040 <pedro.loureiro7@hotmail.com>
2019-04-18 23:50:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function takeANumber(katzDeliLine,name)
 
 function nowServing(katzDeliLine)
 {   
-    if(katzDeliLine.length != 0)
+    if(katzDeliLine.length !== 0)
     {
         return "Currently serving " + katzDeliLine.shift(0);
     }
